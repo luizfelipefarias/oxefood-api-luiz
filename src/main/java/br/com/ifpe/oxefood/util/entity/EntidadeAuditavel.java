90172bb3ce09793ccb430ca0a189860e1bef1eaf
luizfelipefarias <luizfelipefariasbraz@gmail.com>
2025-09-17 16:41:59 -0300

    Criação da implementação de cliente

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
@@ -2,21 +2,43 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.time.LocalDate;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class EntidadeAuditavel extends EntidadeNegocio {
-    
-    private Long versao;
 
-    private LocalDate dataCriacao;
+   @JsonIgnore
+   @Version
+   private Long versao;
 
-    private LocalDate dataUltimaModificacao;
+   @JsonIgnore
+   @CreatedDate
+   private LocalDate dataCriacao;
 
-    private Long criadoPor; // Id do usuário que o criou
+   @JsonIgnore
+   @LastModifiedDate
+   private LocalDate dataUltimaModificacao;
 
-    private Long ultimaModificacaoPor; // Id do usuário que fez a última alteração
+   @JsonIgnore
+   @Column
+   private Long criadoPor; // Id do usuário que o criou
+
+   @JsonIgnore
+   @Column
+   private Long ultimaModificacaoPor; // Id do usuário que fez a última alteração
 
 }
